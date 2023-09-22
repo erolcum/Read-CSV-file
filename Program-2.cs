c6231dede33202d2fb64756412ad5e0e0235fafe
Erol Iscioglu <110387801+erolcum@users.noreply.github.com>
2023-09-22 17:23:02 +0300

    Add files via upload

---
+++ Program-2.cs
@@ -0,0 +1,35 @@
+﻿
+
+class Program
+{
+    public static void Main()
+    {
+        string filePath =
+        @"test.txt";
+        StreamReader reader = null;
+        if (File.Exists(filePath))
+        {
+            reader = new StreamReader(File.OpenRead(filePath));
+            List<string> listA = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                foreach (var item in values)
+                {
+                    listA.Add(item);
+                }
+                foreach (var coloumn1 in listA)
+                {
+                    Console.WriteLine(coloumn1);
+                }
+                listA.Clear();
+            }
+        }
+        else
+        {
+            Console.WriteLine("File doesn't exist");
+        }
+        Console.ReadLine();
+    }
+}

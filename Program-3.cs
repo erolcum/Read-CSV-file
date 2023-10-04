55c5d168302bc863cf7a877ca2de88ed73cc695f
Erol Iscioglu <110387801+erolcum@users.noreply.github.com>
2023-10-04 11:32:03 +0300

    Add files via upload

---
+++ Program-3.cs
@@ -0,0 +1,33 @@
+﻿
+using System.IO;
+
+class Program
+{
+    public static void Main()
+    {
+        string filePath = @"test.txt";
+        Dictionary<string, string> programConfigList =
+            new Dictionary<string, string>();
+        programConfigList.Add("PrinterPort", "9100");
+        programConfigList.Add("PrinterIp", "192.168.1.50");
+        programConfigList.Add("PlcPort", "COM2");
+
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var item in programConfigList)
+                {     
+                    writer.WriteLine( item.Key + "," + item.Value );
+                }
+            }
+
+            Console.WriteLine("PrinterPort: " + programConfigList["PrinterPort"]);
+        }
+        catch (Exception error)
+        {
+            Console.WriteLine(error.Message);
+        }
+        Console.ReadKey();
+    }
+}
